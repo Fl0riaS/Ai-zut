1996a3d3b1d1fb66e50050231fb78537ade4d998
Fl0riaS <jakubznyk@protonmail.com>
2023-10-15 21:10:50 +0200

    lab02: edit tasks

--- src/lab02/script.js
+++ src/lab02/script.js
@@ -34,10 +34,14 @@ class TaskList {
       const newRow = this.tableBody.insertRow()
 
       // Insert cell with content to row
-      newRow.insertCell().append(document.createTextNode(task.content))
+      const contentCell = newRow.insertCell()
+      contentCell.classList.add('content-cell')
+      contentCell.append(document.createTextNode(task.content))
 
       // Insert cell with date to row
-      newRow.insertCell().append(document.createTextNode(task.date))
+      const dateCell = newRow.insertCell()
+      dateCell.classList.add('date-cell')
+      dateCell.append(document.createTextNode(task.date))
 
       // Insert delete button to row
       const deleteButton = document.createElement('button')
@@ -49,6 +53,10 @@ class TaskList {
 
       // Attach delete handler
       deleteButton.addEventListener('click', handleDelete)
+
+      // Attach edit handler
+      contentCell.addEventListener('click', handleEditStart)
+      dateCell.addEventListener('click', handleEditStart)
     }
   }
 
@@ -78,22 +86,27 @@ class TaskList {
   }
 
   editTask(id, taskData) {
+    // Find task in list
+    const taskIndex = this.tasks.findIndex(task => task.id === id)
+
     // Edit task in list
-    this.tasks = this.tasks.map(task => {
-      if (task.id !== id) {
-        return { ...task, ...taskData }
-      }
-      return task
-    })
+    this.tasks[taskIndex] = { ...this.tasks[taskIndex], ...taskData }
 
     // Edit task in local storage
-    window.localStorage.setItem
+    window.localStorage.setItem(
+      this.tasks[taskIndex].id,
+      JSON.stringify(this.tasks[taskIndex])
+    )
 
     this.draw()
   }
 
   search(searchText) {
     this.filteredTasks = tasks.filter(task => task.content.includes(searchText))
+  }
+
+  getTaskById(id) {
+    return this.tasks.find(task => task.id === id)
   }
 }
 
@@ -131,11 +144,85 @@ const handleSearch = event => {
 
 const handleDelete = event => {
   const nodeId = event.target.parentNode.parentNode.id
-  const taskId = nodeId.split('-')[1]
 
-  taskList.removeTask(taskId)
+  taskList.removeTask(cutTaskId(nodeId))
 }
 
+const handleEditSave = event => {
+  // Check if something other than edit input is clicked
+  if (!event.target.classList.contains('edit-task-input')) {
+    const editInputReference =
+      document.getElementsByClassName('edit-task-input')[0]
+
+    if (!editInputReference) {
+      return
+    }
+
+    // If cell is clicked, check if active input is not the same as clicked cell
+    if (
+      event.target.parentNode.id === editInputReference.parentNode.parentNode.id
+    ) {
+      return
+    }
+
+    // Get task id from table row
+    const taskId = cutTaskId(editInputReference.parentNode.parentNode.id)
+
+    // Validate and edit task
+    if (editInputReference.type === 'text') {
+      if (editInputReference.value.length < 3) {
+        return alert('Content is too short')
+      }
+      if (editInputReference.value.length > 255) {
+        return alert('Content is too long')
+      }
+      taskList.editTask(taskId, { content: editInputReference.value })
+    } else {
+      if (Date.now() > new Date(editInputReference.value).getTime()) {
+        return alert('Date must be in future')
+      }
+
+      taskList.editTask(taskId, { date: editInputReference.value })
+    }
+
+  }
+}
+
+const handleEditStart = event => {
+  // Check if input is not clicked again
+  if (event.target.tagName !== 'INPUT') {
+    // Check if any other input is not clicked
+    const editInputsReference =
+      document.getElementsByClassName('edit-task-input')
+    if (editInputsReference.length > 0) {
+      return
+    }
+
+    // Get task from task list
+    const task = taskList.getTaskById(cutTaskId(event.target.parentNode.id))
+
+    // Check if input type should be text or date
+    const inputType = event.target.classList.contains('content-cell')
+      ? 'text'
+      : 'date'
+
+    // Create input and set value to previous one
+    const newInput = document.createElement('input')
+    newInput.type = inputType
+    newInput.value = task[inputType === 'text' ? 'content' : 'date']
+    newInput.classList.add('edit-task-input')
+
+    // Append input to dom
+    event.target.removeChild(event.target.childNodes[0])
+    event.target.append(newInput)
+  }
+}
+// #endregion
+
+// #region HELPERS
+const cutTaskId = id => {
+  return id.split('-')[1]
+}
 // #endregion
 
 // #region MAIN
@@ -144,7 +231,10 @@ const taskList = new TaskList()
 // Attach event listeners
 const taskForm = document.getElementById('add-task-form')
 taskForm.addEventListener('submit', handleFormSubmit)
+
 const searchTextField = document.getElementById('search-text-field')
 searchTextField.addEventListener('input', handleSearch)
+
+document.addEventListener('click', handleEditSave)
 
 // #endregion
