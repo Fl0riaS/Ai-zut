38012a581f5c4671d3ae08c0fe65122161be6e98
Fl0riaS <jakubznyk@protonmail.com>
2023-10-15 19:22:04 +0200

    lab02: adding and deleting tasks

--- src/lab02/script.js
+++ src/lab02/script.js
@@ -1,6 +1,7 @@
 class TaskList {
+  tableBody = undefined
   tasks = []
-  searchText = ''
+  filteredTasks = []
 
   constructor() {
     // Get tasks from local storage
@@ -10,13 +11,45 @@ class TaskList {
       this.tasks.push(JSON.parse(task))
     }
 
-    console.log(this.tasks)
+    // Save reference to table
+    this.tableBody = document
+      .getElementById('todo-table')
+      .getElementsByTagName('tbody')[0]
 
     this.draw()
   }
 
   draw() {
-    // TODO: generate content inside list
+    // Clear previous table
+    const initialLength = this.tableBody.rows.length
+    for (let i = 0; i < initialLength - 1; i++) {
+      this.tableBody.removeChild(this.tableBody.rows[1])
+    }
+
+    const isSearched = this.filteredTasks.length > 0
+
+    // Generate content inside table
+    for (const task of isSearched ? this.filteredTasks : this.tasks) {
+      // Insert new row to table
+      const newRow = this.tableBody.insertRow()
+
+      // Insert cell with content to row
+      newRow.insertCell().append(document.createTextNode(task.content))
+
+      // Insert cell with date to row
+      newRow.insertCell().append(document.createTextNode(task.date))
+
+      // Insert delete button to row
+      const deleteButton = document.createElement('button')
+      deleteButton.innerHTML = 'X'
+      newRow.insertCell().append(deleteButton)
+
+      // Assign task id to row
+      newRow.setAttribute('id', `task-${task.id}`)
+
+      // Attach delete handler
+      deleteButton.addEventListener('click', handleDelete)
+    }
   }
 
   addTask(content, date) {
@@ -58,9 +91,13 @@ class TaskList {
 
     this.draw()
   }
+
+  search(searchText) {
+    this.filteredTasks = tasks.filter(task => task.content.includes(searchText))
+  }
 }
 
-// Handlers
+// #region HANDLERS
 const handleFormSubmit = event => {
   event.preventDefault()
 
@@ -81,15 +118,33 @@ const handleFormSubmit = event => {
     return alert('Content is too long')
   }
 
+  // Reset inputs
+  event.target.reset()
+
   // Add task to list
   taskList.addTask(content, date)
-
-  console.log(taskList)
 }
 
-// Main code
+const handleSearch = event => {
+  taskList.setSearchText(event.target.value)
+}
+
+const handleDelete = event => {
+  const nodeId = event.target.parentNode.parentNode.id
+  const taskId = nodeId.split('-')[1]
+
+  taskList.removeTask(taskId)
+}
+
+// #endregion
+
+// #region MAIN
 const taskList = new TaskList()
 
-// Attach event listener
+// Attach event listeners
 const taskForm = document.getElementById('add-task-form')
 taskForm.addEventListener('submit', handleFormSubmit)
+const searchTextField = document.getElementById('search-text-field')
+searchTextField.addEventListener('input', handleSearch)
+
+// #endregion
